8fe2732bb1f6632b1b4347c069deecac9854091c
El Yuk <josue.rojas@ciencias.unam.mx>
2022-10-16 14:31:29 -0500

    Hop bridge implement

--- src/hopScripts/bridge.js
+++ src/hopScripts/bridge.js
@@ -1,19 +1,21 @@
-import { Hop } from '@hop-protocol/sdk'
+import { Hop, Chain } from '@hop-protocol/sdk'
 import { providers } from 'ethers'
 
-export default async function EthArbBridge() {
+
+export default async function main() {
   const provider = new providers.Web3Provider(window.ethereum, 'any')
   const signer = provider.getSigner()
   const hop = new Hop('mainnet', signer)
   const bridge = hop.bridge('ETH')
-  const { totalFee, estimatedReceived } = await bridge.getSendData('0', 'ethereum', 'arbitrum')
-  const needsApproval = await bridge.needsApproval('0', 'ethereum')
+  const { totalFee, estimatedReceived } = await bridge.getSendData('100000000000000', 'ethereum', 'polygon')
+  const needsApproval = await bridge.needsApproval('100000000000000', 'ethereum')
   if (needsApproval) {
-    const tx = await bridge.sendApproval('0', 'ethereum', 'arbitrum')
-    await tx.wait()
-  }
-  const tx = await bridge.send('0', 'ethereum', 'arbitrum', {
-    relayerFee: totalFee
-  })
-  console.log(tx.hash)
+      const tx = await bridge.sendApproval('100000000000000', 'ethereum', 'polygon')
+      await tx.wait()
+    }
+    const tx = await bridge.send('100000000000000', Chain.Ethereum, Chain.Arbitrum, {
+        relayerFee: totalFee,
+        recipient: '0xA9783975c88fF60fB6B3c7D50090d1d1fE989170'
+    })
+    console.log(tx.hash)
 }
